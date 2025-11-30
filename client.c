745812b82a3d296ce4c0b5f615b7eddeddf8ae2f
Alt+F4 <133301559+Majeonghun@users.noreply.github.com>
2025-12-01 03:39:16 +0900

    Update client.c

--- client.c
+++ client.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -14,52 +15,60 @@ typedef struct {
     char content[256];
 } mymsgbuf;
 
-int main() {
-    int msgid;
-    mymsgbuf msg;
-    int client_id;
-    int bid;
+int client_id;
+int msgid;
 
-    // Connect to the message queue
+// Thread to receive winner notification
+void* winner_listener(void* arg) {
+    mymsgbuf msg;
+
+    while (1) {
+        if (msgrcv(msgid, &msg, sizeof(msg.content), client_id, 0) >= 0) {
+            printf("\n===== AUCTION RESULT =====\n");
+            printf("%s\n", msg.content);
+            printf("==========================\n");
+            exit(0);
+        }
+    }
+    return NULL;
+}
+
+int main() {
+    mymsgbuf msg;
+    int bid;
+    pthread_t listener;
+
     msgid = msgget(KEY, 0666);
-    if(msgid < 0) {
+    if (msgid < 0) {
         perror("msgget");
         exit(1);
     }
 
-    // Enter client ID
-    printf("Enter your Client ID (integer): ");
-    if(scanf("%d", &client_id) != 1) {
-        printf("Invalid ID\n");
-        exit(1);
-    }
-    getchar(); // consume newline
+    printf("Enter your Client ID: ");
+    scanf("%d", &client_id);
+    getchar();
 
-    printf("\nAuction started! You can bid anytime by entering a value.\n");
-    printf("Format: <bid_amount>\n");
-    printf("Example: 100\n");
+    pthread_create(&listener, NULL, winner_listener, NULL);
 
-    while(1) {
-        printf("\nEnter your bid (or 0 to skip): ");
-        if(scanf("%d", &bid) != 1) {
-            printf("Invalid input. Try again.\n");
-            while(getchar() != '\n'); // clear input buffer
+    printf("Auction started. You can bid anytime.\n");
+
+    while (1) {
+        printf("\nEnter your bid (0 to keep previous): ");
+        if (scanf("%d", &bid) != 1) {
+            while (getchar() != '\n');
             continue;
         }
-        getchar(); // consume newline
+        getchar();
 
-        if(bid < 0) {
+        if (bid < 0) {
             printf("Bid cannot be negative.\n");
             continue;
         }
 
-        // Prepare message: "<ClientID> <Bid>"
         msg.mtype = 2;
         snprintf(msg.content, sizeof(msg.content), "%d %d", client_id, bid);
 
-        if(msgsnd(msgid, &msg, sizeof(msg.content), 0) < 0) {
-            perror("msgsnd");
-        } else {
+        if (msgsnd(msgid, &msg, sizeof(msg.content), 0) >= 0) {
             printf("Bid submitted: %d\n", bid);
         }
     }
